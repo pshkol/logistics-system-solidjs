b95617818ab75f72009b22bf3d34780f831de5e1
Pavlo Shkolnyy <pavlo@sodtrack.com>
2024-06-17 16:03:55 -0300

    :bug: Fix strange prod error in dashboard when change date

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createSignal, createResource } from "solid-js";
+import { createSignal, createResource, Show } from "solid-js";
 import { TextField, TextFieldRoot } from "~/components/ui/textfield";
 import { getClientsDebt } from "~/actions/client/get-clients-debt";
 import { getDriversDebt } from "~/actions/driver/get-drivers-debt";
@@ -49,33 +49,39 @@ export default function Home() {
         </TextFieldRoot>
       </section>
       <section class={"mt-5 grid grid-cols-3 gap-2"}>
-        <div class={"rounded-lg bg-neutral-50 p-2 shadow-md"}>
-          <p class={"text-sm font-semibold"}>Plata real</p>
-          <span>
-            {new Intl.NumberFormat("es-AR", {
-              style: "currency",
-              currency: "ARS",
-            }).format(realMoney() ?? 0)}
-          </span>
-        </div>
-        <div class={"rounded-lg bg-neutral-50 p-2 shadow-md"}>
-          <p class={"text-sm font-semibold"}>Deuda de clientes</p>
-          <span>
-            {new Intl.NumberFormat("es-AR", {
-              style: "currency",
-              currency: "ARS",
-            }).format(clientsDebt() ?? 0)}
-          </span>
-        </div>
-        <div class={"rounded-lg bg-neutral-50 p-2 shadow-md"}>
-          <p class={"text-sm font-semibold"}>Deuda a conductores</p>
-          <span>
-            {new Intl.NumberFormat("es-AR", {
-              style: "currency",
-              currency: "ARS",
-            }).format(driversDebt() ?? 0)}
-          </span>
-        </div>
+        <Show when={realMoney()}>
+          <div class={"rounded-lg bg-neutral-50 p-2 shadow-md"}>
+            <p class={"text-sm font-semibold"}>Plata real</p>
+            <span>
+              {new Intl.NumberFormat("es-AR", {
+                style: "currency",
+                currency: "ARS",
+              }).format(realMoney() ?? 0)}
+            </span>
+          </div>
+        </Show>
+        <Show when={clientsDebt()}>
+          <div class={"rounded-lg bg-neutral-50 p-2 shadow-md"}>
+            <p class={"text-sm font-semibold"}>Deuda de clientes</p>
+            <span>
+              {new Intl.NumberFormat("es-AR", {
+                style: "currency",
+                currency: "ARS",
+              }).format(clientsDebt() ?? 0)}
+            </span>
+          </div>
+        </Show>
+        <Show when={driversDebt()}>
+          <div class={"rounded-lg bg-neutral-50 p-2 shadow-md"}>
+            <p class={"text-sm font-semibold"}>Deuda a conductores</p>
+            <span>
+              {new Intl.NumberFormat("es-AR", {
+                style: "currency",
+                currency: "ARS",
+              }).format(driversDebt() ?? 0)}
+            </span>
+          </div>
+        </Show>
       </section>
     </main>
   );
